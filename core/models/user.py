3df8ff44a396c528c30ef3ef3cef7b9b8467ebc0
Maktolik <maktolik97@gmail.com>
2024-07-02 16:20:10 +0300

    post-user relationship

--- core/models/user.py
+++ core/models/user.py
@@ -1,9 +1,16 @@
-from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import TYPE_CHECKING
+
+from sqlalchemy import String, Integer
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
+
+if TYPE_CHECKING:
+    from .post import Post
 
 
 class User(Base):
     # __tablename__ = "users"
     username: Mapped[str] = mapped_column(String(12), unique=True)
+
+    posts: Mapped[list["Post"]] = relationship(back_populates="user")
